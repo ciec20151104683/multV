5be705ae984b46d03f1778e0077b2df403ba06e7
ciec20151104683 <1902522846@qq.com>
2017-06-22 16:18:38 +0800

    完成了加数据库的基本配置

---
+++ SQLite-Bridge.h
@@ -0,0 +1,14 @@
+//
+//  SQLite-Bridge.h
+//  multV
+//
+//  Created by s20151104683 on 17/6/22.
+//  Copyright © 2017年 s20151104683. All rights reserved.
+//
+
+#ifndef SQLite_Bridge_h
+#define SQLite_Bridge_h
+#import "sqlite.h"
+
+
+#endif /* SQLite_Bridge_h */
